ffe6a69730e75152d97dd29b2ffddd05ba6b0ce5
Renato <jefferson.renato07@gmail.com>
2023-02-08 09:45:16 -0300

    new placeholder

--- src/componentes/Cabecalho/Cabecalho.jsx
+++ src/componentes/Cabecalho/Cabecalho.jsx
@@ -1,21 +1,34 @@
-import logo from './logo.png'
-import search from './search.png'
-import styles from './Cabecalho.module.scss'
+import logo from "./logo.png";
+import search from "./search.png";
+import styles from "./Cabecalho.module.scss";
+import { useState } from "react";
 
 const Cabecalho = () => {
+  const [placeholder, setPlaceholder] = useState("O que você procura?");
+
+  function focado() {
+    setPlaceholder("");
+  }
+
+  function desfocado() {
+    setPlaceholder("O que você procura?");
+  }
+
   return (
     <header className={styles.cabecalho}>
       <img src={logo} alt="Logo do Alura Space" />
       <div className={styles.cabecalho__container}>
         <input
-        className={styles.cabecalho__input}
-        type="text"
-        placeholder="O que você procura?"
+          className={styles.cabecalho__input}
+          type="text"
+          placeholder={placeholder}
+          onFocus={focado}
+          onBlur={desfocado}
         />
         <img src={search} alt="Ícone de lupa" />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Cabecalho
+export default Cabecalho;
