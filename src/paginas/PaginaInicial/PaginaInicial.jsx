e9b45943be5006b666d3e81497e77dfce0655844
Renato <jefferson.renato07@gmail.com>
2023-02-08 08:13:19 -0300

    Aula 2

--- src/paginas/PaginaInicial/PaginaInicial.jsx
+++ src/paginas/PaginaInicial/PaginaInicial.jsx
@@ -1,9 +1,20 @@
-import { Cabecalho } from "./../../componentes/Cabecalho";
+import { Cabecalho } from "../../componentes/Cabecalho";
+import { Menu } from "../../componentes/Menu";
+import { Banner } from "../../componentes/Banner";
+import styles from "./PaginaInicial.module.scss";
 
 const PaginaInicial = () => {
   return (
-  <Cabecalho />
-  )
-}
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <Banner />
+        </section>
+      </main>
+    </>
+  );
+};
 
 export default PaginaInicial;
